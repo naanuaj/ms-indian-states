6d134f1aacf9658a174d91ebf28e85bed41e219c
Ajay <ajayaju23@outlook.com>
2022-07-10 01:04:17 +0530

    Spring Boot Initializer Raw Project Push

---
+++ MicroService-IndianStates/src/main/java/com/ajdev/ind/states/MicroServiceIndianStatesApplication.java
@@ -0,0 +1,13 @@
+package com.ajdev.ind.states;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceIndianStatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceIndianStatesApplication.class, args);
+	}
+
+}
---
+++ MicroService-IndianStates/src/test/java/com/ajdev/ind/states/MicroServiceIndianStatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajdev.ind.states;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceIndianStatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
